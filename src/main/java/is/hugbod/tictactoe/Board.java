3413553b2238ecff75e112c55e9912b3a5d30dc2
Arnór Brynjólfsson <arnorbry@gmail.com>
2015-11-02 13:30:41 +0000

    Added functions to Board.java

--- src/main/java/is/hugbod/tictactoe/Board.java
+++ src/main/java/is/hugbod/tictactoe/Board.java
@@ -1,5 +1,7 @@
 package is.hugbod.tictactoe;
 
+
+
 public class Board{
 	char[][] theBoard;
 	char player = 'x'; 
@@ -7,5 +9,34 @@ public class Board{
 		theBoard  = new char[3][3];
 	}
 
-	
+	public void FillSquare(){
+		
+	}
+
+	public boolean CheckForWin(){
+		if(CheckHorizontal() == true || CheckDiagonal() == true || CheckVertical == true){
+			return true;
+		}
+		return false
+	}
+
+	public boolean CheckForFullBoard(){
+		return false;
+	}
+
+	public boolean CheckHorizontal(){
+		return true;
+	}
+
+	public boolean CheckDiagonal(){
+		return false;
+	}
+
+	public boolean CheckVertical(){
+		return false;
+	}
+
+	public void Reset(){
+		
+	}
 }
