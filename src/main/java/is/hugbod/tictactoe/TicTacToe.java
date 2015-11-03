0b1a2b8d2bfb52fb85e99e7db933becb07e48bc3
Stinni <kristinnf13@ru.is>
2015-11-03 17:52:09 +0000

    Fixed the rest of the checkstyle errors. Test class and file renamed

--- src/main/java/is/hugbod/tictactoe/TicTacToe.java
+++ src/main/java/is/hugbod/tictactoe/TicTacToe.java
@@ -1,31 +1,39 @@
 package is.hugbod.tictactoe;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TicTacToe implements ActionListener {
 
-	static Service service;
-	JFrame frame;
-	JButton[][] board;
-	boolean singleplayer = false;
+	private static Service service;
+	private JFrame frame;
+	private JButton[][] board;
+	private boolean singleplayer = false;
 
 	private final int boardSize = 3;
-	
+	private final int minFrameSizeX = 550;
+	private final int minAndMaxFrameSizeY = 500;
+	private final int maxFrameSizeX = 600;
+	private final int locationX = 500;
+	private final int locationY = 200;
+
 	public TicTacToe() {
 		frame = new JFrame("Tic Tac Toe by Hugboð");
-		frame.setMinimumSize(new Dimension(550, 500));
-		frame.setMaximumSize(new Dimension(600, 500));
+		frame.setMinimumSize(new Dimension(minFrameSizeX, minAndMaxFrameSizeY));
+		frame.setMaximumSize(new Dimension(maxFrameSizeX, minAndMaxFrameSizeY));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new GridLayout(boardSize, boardSize));
-		frame.setLocation(500, 200);
+		frame.setLocation(locationX, locationY);
 		frame.setVisible(true);
 		service = new Service();
 		board = new JButton[boardSize][boardSize];
-		for(int i = 0; i < boardSize; i++) {
-			for(int j = 0; j < boardSize; j++) {
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
 				board[i][j] = new JButton("");
 				frame.getContentPane().add(board[i][j]);
 				board[i][j].addActionListener(this);
@@ -36,62 +44,57 @@ public class TicTacToe implements ActionListener {
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e)
-	{
+	public final void actionPerformed(final ActionEvent e) {
 		boolean legal = false;
 		for (int i = 0; i < boardSize; i++) {
 			for (int j = 0; j < boardSize; j++) {
-				if(e.getSource()==board[i][j]) {
-				legal = service.isLegalTic(i, j);
-				service.updateBoard(i, j);
+				if (e.getSource() == board[i][j]) {
+					legal = service.isLegalTic(i, j);
+					service.updateBoard(i, j);
 				}
 			}
 		}
 		display();
 		endConditions();
 
-		if(legal && singleplayer && !service.checkForWin() && !service.checkForFullBoard()) {
+		if (legal && singleplayer && !service.checkForWin() && !service.checkForFullBoard()) {
 			// Let the AI play one move for 'O'
 			aiTic();
 		}
 	}
 
-	private void displayResult(char result) {
+	private void displayResult(final char result) {
 		String resultmsg;
-		if(result == 'D') {
+		if (result == 'D') {
 			resultmsg = "Match is draw!";
-		}
-		else if(result == 'X') {
+		} else if (result == 'X') {
 			resultmsg = "Circle wins!";
-		}
-		else if(result == 'O') {
+		} else if (result == 'O') {
 			resultmsg = "Cross wins!";
-		}
-		else {
+		} else {
 			resultmsg = "WHAT?";
 		}
 		Object[] options = {"Rematch", "No, thanks"};
 		int n = JOptionPane.showOptionDialog(
 			null,
-			resultmsg, 
+			resultmsg,
 			"Result",
-			JOptionPane.YES_NO_OPTION, 
+			JOptionPane.YES_NO_OPTION,
 			JOptionPane.QUESTION_MESSAGE,
 			null,
 			options,
 			options[1]);
-		if(n == 0) {
+		if (n == 0) {
 			reset();
-		}
-		else {
+		} else {
 			frame.dispose();
 		}
 	}
 
 	private void display() {
-		for(int i = 0; i < boardSize; i++) {
-			for(int j = 0; j < boardSize; j++) {
-				board[i][j].setText("" + service.getBoard(i,j));
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				board[i][j].setText("" + service.getBoard(i, j));
 			}
 		}
 	}
@@ -106,35 +109,33 @@ public class TicTacToe implements ActionListener {
 		display();
 		endConditions();
 	}
-	
+
 	// check for end conditions
 	private void endConditions() {
 		// check if there is a winner
-		if(service.checkForWin()) {
+		if (service.checkForWin()) {
 			displayResult(service.getCurrentPlayer());
-		}
-		// check if there is a draw (full board and no winner)
-		else if(service.checkForFullBoard()) {
+		} else if (service.checkForFullBoard()) {
+			// check if there is a draw (full board and no winner)
 			displayResult('D');
 		}
 	}
 
-	
+
 	private void playingMode() {
 		Object[] options = {"Single Player", "2 Player"};
 		int n = JOptionPane.showOptionDialog(
 			null,
-			"vs computer or player?", 
+			"vs computer or player?",
 			"Result",
-			JOptionPane.YES_NO_OPTION, 
+			JOptionPane.YES_NO_OPTION,
 			JOptionPane.QUESTION_MESSAGE,
 			null,
 			options,
 			options[1]);
-		if(n == 0) {
+		if (n == 0) {
 			singleplayer = true;
-		}
-		else {
+		} else {
 			singleplayer = false;
 		}
 	}
--- src/test/java/is/hugbod/tictactoe/TicTacToeTest.java
+++ src/test/java/is/hugbod/tictactoe/TicTacToeTest.java
@@ -1,113 +1,115 @@
 package is.hugbod.tictactoe;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
-public class test{
+public class TicTacToeTest {
 
 	private final int boardSize = 3;
 
-	public static void main(String args[]){
+	public static void main(final String[] args) {
 		org.junit.runner.JUnitCore.main("is.hugbod.tictactoe.test");
 	}
-	
+
 	// Test that board is empty / reset
 	@Test
-	public void testResetBoard(){
+	public final void testResetBoard() {
 		Service test = new Service();
 		test.resetBoard();
-		for(int i = 0; i < boardSize; i++) {
-			for(int j = 0; j < boardSize; j++) {
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
 				// Assert for each square that they are '-'
-				assertEquals('-', test.getBoard(i,j));
-			} 
+				assertEquals('-', test.getBoard(i, j));
+			}
 		}
 	}
-	
+
 	// Test for legal tic
 	@Test
-	public void testIsLegalTic(){
+	public final void testIsLegalTic() {
 		Service test = new Service();
-		assertTrue(test.isLegalTic(1,1));
+		assertTrue(test.isLegalTic(1, 1));
 	}
 
 	// Test for illegal tic
 	@Test
-	public void testIsLegalTic2(){
+	public final void testIsLegalTic2() {
 		Service test = new Service();
-		test.setBoard(1,1,'X');
-		assertFalse(test.isLegalTic(1,1));
+		test.setBoard(1, 1, 'X');
+		assertFalse(test.isLegalTic(1, 1));
 	}
 
 	@Test
-	public void checkForFullBoard(){
+	public final void checkForFullBoard() {
 		Service test = new Service();
 		// Empty board tested
 		assertFalse(test.checkForFullBoard());
-		
+
 		// Board filled with 'X'
-		for(int i = 0; i < boardSize; i++) {
-			for(int j = 0; j < boardSize; j++) {
-				test.setBoard(i,j,'X');
-			} 
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				test.setBoard(i, j, 'X');
+			}
 		}
 		assertTrue(test.checkForFullBoard());
 	}
 
 	// Test for horizontal win
 	@Test
-	public void testCheckForWin(){
+	public final void testCheckForWin() {
 		Service test = new Service();
-		test.setBoard(1,0,'X');
-		test.setBoard(1,1,'X');
-		test.setBoard(1,2,'X');
+		test.setBoard(1, 0, 'X');
+		test.setBoard(1, 1, 'X');
+		test.setBoard(1, 2, 'X');
 		// win for 'X'
 		assertTrue(test.checkForWin());
 
-		test.setBoard(1,0,'O');
-		test.setBoard(1,1,'O');
-		test.setBoard(1,2,'O');
+		test.setBoard(1, 0, 'O');
+		test.setBoard(1, 1, 'O');
+		test.setBoard(1, 2, 'O');
 		// win for 'O'
 		assertTrue(test.checkForWin());
 	}
 
 	// Test for vertical win
 	@Test
-	public void testCheckForWin2(){
+	public final void testCheckForWin2() {
 		Service test = new Service();
-		test.setBoard(0,0,'X');
-		test.setBoard(1,0,'X');
-		test.setBoard(2,0,'X');
+		test.setBoard(0, 0, 'X');
+		test.setBoard(1, 0, 'X');
+		test.setBoard(2, 0, 'X');
 		// win for 'X'
 		assertTrue(test.checkForWin());
 
-		test.setBoard(0,0,'O');
-		test.setBoard(1,0,'O');
-		test.setBoard(2,0,'O');
+		test.setBoard(0, 0, 'O');
+		test.setBoard(1, 0, 'O');
+		test.setBoard(2, 0, 'O');
 		// win for 'O'
 		assertTrue(test.checkForWin());
 	}
 
 	// Test for diagonal win
 	@Test
-	public void testCheckForWin3(){
+	public final void testCheckForWin3() {
 		Service test = new Service();
-		test.setBoard(0,0,'X');
-		test.setBoard(1,1,'X');
-		test.setBoard(2,2,'X');
+		test.setBoard(0, 0, 'X');
+		test.setBoard(1, 1, 'X');
+		test.setBoard(2, 2, 'X');
 		// win for 'X'
 		assertTrue(test.checkForWin());
 
-		test.setBoard(2,0,'O');
-		test.setBoard(1,1,'O');
-		test.setBoard(0,2,'O');
+		test.setBoard(2, 0, 'O');
+		test.setBoard(1, 1, 'O');
+		test.setBoard(0, 2, 'O');
 		// win for 'O'
 		assertTrue(test.checkForWin());
 	}
 
 	// Test for no win
 	@Test
-	public void testCheckForWin4(){
+	public final void testCheckForWin4() {
 		Service test = new Service();
 		// Empty board tested
 		assertFalse(test.checkForWin());
@@ -115,30 +117,30 @@ public class test{
 
 	// Test correct update of board
 	@Test
-	public void testUpdateBoard(){
+	public final void testUpdateBoard() {
 		Service test = new Service();
 		// get the current player
 		char player = test.getCurrentPlayer();
 		// update one field with current player mark
-		test.updateBoard(1,1);
+		test.updateBoard(1, 1);
 		assertEquals(player, test.getBoard(1, 1));
 		// update another field with next player
 		player = test.getCurrentPlayer();
-		test.updateBoard(0,1);
+		test.updateBoard(0, 1);
 		assertEquals(player, test.getBoard(0, 1));
 		// try illegal move (same as previous player)
 		// No need to update player here since the field should NOT change
-		test.updateBoard(0,1);
+		test.updateBoard(0, 1);
 		assertEquals(player, test.getBoard(0, 1));
 		// try another field!
 		player = test.getCurrentPlayer();
-		test.updateBoard(2,1);
+		test.updateBoard(2, 1);
 		assertEquals(player, test.getBoard(2, 1));
 	}
 
 	// Test for ai
 	@Test
-	public void testaiTic(){
+	public final void testaiTic() {
 		Service test = new Service();
 		char player = test.getCurrentPlayer();
 		test.aiTic();
