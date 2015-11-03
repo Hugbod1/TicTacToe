c81a7cd1b72203cadf096f2f7ef826f67538a557
Stinni <kristinnf13@ru.is>
2015-11-03 10:26:30 +0000

    Added checkstyle doc and apply cmd

--- src/main/java/is/hugbod/tictactoe/Service.java
+++ src/main/java/is/hugbod/tictactoe/Service.java
@@ -1,25 +1,25 @@
 package is.hugbod.tictactoe;
 
-public class Service
-{
+public class Service {
 	char[][] board;
 	char player;
+	int size = 3;
 
 	public Service() {
-		board = new char[3][3];
+		board = new char[size][size];
 		player = 'X';
 		ResetBoard();
 	}
 
 	public void UpdateBoard(int row, int col) {
-		if(!IsLegalTic(row, col)) {
+		if (!IsLegalTic(row, col)) {
 			// not a legal move
 			return;
 		}
 		board[row][col] = player;
 
 		// swap current player
-		if(player == 'X') {
+		if (player == 'X') {
 			player = 'O';
 		}
 		else {
@@ -32,49 +32,49 @@ public class Service
 	}
 
 	public boolean CheckForFullBoard() {
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				if(board[i][j] == '-') {
-				return false;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (board[i][j] == '-') {
+					return false;
 				}
-			} 
+			}
 		}
-	return true;
+		return true;
 	}
-	
+
 	private boolean CheckDiagonal() {
 		return (((board[1][1] != '-') && (((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) ||
 				((board[0][2] == board[1][1]) && (board[1][1] == board[2][0])))));
 	}
-	
+
 	private boolean CheckVertical() {
-		for(int i = 0; i < 3; i++) {
-			if(board[0][i] != '-' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+		for (int i = 0; i < size; i++) {
+			if (board[0][i] != '-' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	private boolean CheckHorizontal() {
-		for(int i = 0; i < 3; i++) {
-			if(board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+		for (int i = 0; i < size; i++) {
+			if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	public boolean IsLegalTic(int row, int col) {
 		return ((board[row][col] != 'X') && (board[row][col] != 'O'));
 	}
-	
+
 	public void ResetBoard() {
 		// Board set to zero
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
 				board[i][j] = '-';
-			} 
+			}
 		}
 	}
 }
